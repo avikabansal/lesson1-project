13ba330808163d255beb27c92a96170f9aab8fe5
avikabansal <avika.artsy@gmail.com>
2025-07-09 19:13:18 +0530

    Add files via upload

---
+++ birthday.py
@@ -0,0 +1,10 @@
+avika=8/11/2013
+mayra=25/11/2013
+sara=7/11/2013
+shivanshi=26/11/2012
+tisya=22/5/2013
+print("my birthday is",avika)
+print("my birthday is",mayra)
+print("my birthday is",sara)
+print("my birthday is",shivanshi)
+print("my birthday is",tisya)
