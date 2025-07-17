e647d1600d41f638b4576f792f7ff63f06a848b6
avikabansal <avika.artsy@gmail.com>
2025-07-17 19:57:12 +0530

    Add files via upload

---
+++ greater or smaller.py
@@ -0,0 +1,5 @@
+I=int(input("enter a number: "))
+if (I<15):
+    print(I,"is smaller than 15")
+else:
+    print(I,"is greater than 15")
---
+++ profit loss.py
@@ -0,0 +1,7 @@
+actual_cost=100
+selling_cost=80
+if (selling_cost>actual_cost):
+    total = selling_cost-actual_cost
+    print(total,"profit")
+else:
+    print("loss")   
