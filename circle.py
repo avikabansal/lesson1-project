e4a6d53525fc502b7c732c1ba183dbeb30885ab3
avikabansal <avika.artsy@gmail.com>
2025-09-10 21:27:11 +0530

    Add files via upload

---
+++ circle.py
@@ -0,0 +1,7 @@
+def circum(p,q):
+    return p*q
+radius=int(input("enter the radius of the circle: "))
+pi=3.19
+circumference=pi*radius*2
+print(circumference,"is the circumference of the circle of radius",radius)
+           
